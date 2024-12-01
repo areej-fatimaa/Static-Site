de09a22b9ed8fcc3e5c1deb54ff24e50ba2f4d03
Areej Fatima <reejayfatima123@gmail.com>
2024-12-01 15:34:22 +0500

    .

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+  };
+  
+  module.exports = nextConfig;
+  
+  module.exports = {
+    reactStrictMode: true, // Helps with performance by detecting problems
+    swcMinify: true, // Enables minification for JavaScript
+  };
+    
